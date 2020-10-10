8db239f656f3957aa10bb2bc91871a810078defa
franc <franc@DESKTOP-CVO7QAB.local>
2020-10-10 03:16:54 +0200

    FIRST VERSION THAT DOSENT USE FREQUENCY
    AWARENESS BASED ON CORE 0
    i usually add DELAY to the script just for avoid HOTPLUG over use (ACTUALLY 1s)
    
    
    		LITTLE   BIG
    SHUTDOWN CORES:	L:<30% B:<50%
    TURNON CORES: 	L:=>50% B:>=90%

--- sshs.sh
+++ sshs.sh
@@ -3,11 +3,19 @@
 	do	
 	c0=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 	c4=$(cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq)
+	do
+	A0=( $(sed '2q;d' /proc/stat) ) 
+	B0=$(expr "${A0[1]}" + "${A0[2]}" + "${A0[3]}" + "${A0[4]}")
+	sleep 0.2
+	C0=( $(sed '2q;d' /proc/stat) )
+	D0=$(expr "${C0[1]}" + "${C0[2]}" + "${C0[3]}" + "${C0[4]}")
+	let E0=$(((100 * (B0 - D0 - ${A0[4]} + ${C0[4]})) / (B0 - D0)))
 
 	cA3=$(cat /sys/devices/system/cpu/cpu3/online)
 	
 	clear
 	if (("$c0" < "1536000")) #SHUT DOWN SOME Small CORES
+	if (("$E0" < "30")) #SHUT DOWN SOME Small CORES
 	then
 	echo 0 > /sys/devices/system/cpu/cpu2/online
 	echo 0 > /sys/devices/system/cpu/cpu3/online
@@ -15,12 +23,14 @@
 
 
 	if (("$c0" >= "1843200")) #TURN ON THIS Small CORE PARTY
+	if (("$E0" >= "50")) #TURN ON THIS Small CORE PARTY
 	then
 	echo 1 > /sys/devices/system/cpu/cpu2/online
 	echo 1 > /sys/devices/system/cpu/cpu3/online
 	fi
 
 	if (("$c4" < "1536000")) #SHUT DOWN SOME BIG CORES
+	if (("$E0" < "50")) #SHUT DOWN SOME BIG CORES
 	then
 	echo 0 > /sys/devices/system/cpu/cpu5/online
 	echo 0 > /sys/devices/system/cpu/cpu6/online
@@ -28,6 +38,7 @@
 	fi
 
 	if (("$c4" >= "2150000" && cA3 != 0)) #TURN ON THIS BIG CORE PARTY
+	if (("$E0" >= "90")) #TURN ON THIS BIG CORE PARTY
 	then
 	echo 1 > /sys/devices/system/cpu/cpu5/online
 	echo 1 > /sys/devices/system/cpu/cpu6/online
@@ -35,9 +46,5 @@
 	fi
 	
 	sleep 1
+	sleep 0.8
 	done
-
-
-
-# This script will be executed in late_start service mode
-# More info in the main Magisk thread
